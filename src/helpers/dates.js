880ff9cfeeefda47997c190d8990bdcf040975fc
Patrick Nyatindo <nyatindopatrick@gmail.com>
2021-09-27 11:33:35 +0300

    correct birtdates handler and add comments

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -1,36 +1,31 @@
-/* eslint-disable no-nested-ternary */
 /* eslint-disable eqeqeq */
-/* eslint-disable no-extend-native */
-/* eslint-disable func-names */
 const isYear = (date) => date.length === 4;
 const isUnknown = (date) => (date == '99' ? '01' : date);
-const isDashed = (date) => date.includes('-');
+const isDashed = (date) => date && date.includes('-');
+const isDateFormatted = (date) => (date && date.includes('-')) || (date && date.includes('/'));
 const splitAt = (index) => (x) => [x.slice(0, index), x.slice(index)];
+const firstElement = (arr) => (arr.length ? arr[0] : undefined);
 
-Array.prototype.lastElement = function () {
-  return this.length ? this[this.length - 1] : undefined;
-};
-
-// handles dates without formatting, withoutslashes or dashes
-const compactDate = (date, age = null) => {
+// Handles numeric dates
+const compactDate = (date) => {
   const firstFour = date.substr(0, 4);
   const lastFour = date.substr(date.length - 4);
   const thisYear = new Date().getFullYear();
-  const year = Number(firstFour) > 1920 && Number(firstFour) <= thisYear
-    ? firstFour
-    : lastFour;
+  const year = Number(firstFour) > 1920 && Number(firstFour) <= thisYear ? firstFour : lastFour;
 
   const dates = year === firstFour ? lastFour : firstFour;
   const datesArr = splitAt(2)(dates).map((item) => item.replace('99', '01'));
 
-  const formatedDate = datesArr.lastElement > 12 ? datesArr.reverse() : datesArr;
+  const formatedDate = firstElement(datesArr) > 12 ? datesArr.reverse() : datesArr;
 
-  const birthYear = age ? thisYear - Number(age) : year.replace('9999', '1970');
+  const birthYear = year.replace('9999', '1970');
 
-  return `${birthYear}-${formatedDate.join('-')}`;
+  const dateProvided = dates ? formatedDate.join('-') : '01-01';
+
+  return `${birthYear}-${dateProvided}`;
 };
 
-// handles dates with slashes or dashes
+// Handles formatted with slashes or dashes
 const slashedDate = (date) => {
   const splitDate = isDashed(date) ? date.split('-') : date.split('/');
 
@@ -38,25 +33,28 @@ const slashedDate = (date) => {
 
   const year = dateArr.filter((item) => isYear(item)).join('');
   const dates = dateArr.filter((item) => !isYear(item));
-  const formatedDate = dates.lastElement > 12 ? dates.reverse() : dates;
+  const formatedDate = firstElement(dates) > 12 ? dates.reverse() : dates;
 
   return `${year}-${formatedDate.join('-')}`;
 };
 
-const emptyBirthDate = (date, age) => {
-  if (date?.toString().length < 8) return '1970-01-01';
+// Format only birth dates
+const birthDates = (date, age = null) => {
+  const dateString = date?.toString();
+
+  // Calculate birth year and sets 1970 as default if missing
   const year = age ? new Date().getFullYear() - Number(age) : '1970';
-  return `${year}-01-01`;
+  const isDateCorrect = (!dateString || dateString.length < 8) ? `${year}-01-01` : dateString;
+
+  return isDateFormatted(isDateCorrect) ? slashedDate(isDateCorrect) : compactDate(isDateCorrect);
 };
 
-// Format date to YYYY-MM-DD
-const formatDates = (date, age = null) => {
-  if (!date && !age) return '1970-01-01';
-  const dateString = date.toString();
-  if (isDashed(dateString) || dateString.includes('/')) {
-    return slashedDate(dateString);
-  }
-  return compactDate(dateString, age);
+// Format dates other than birth dates
+const formatDates = (date) => {
+  const dateString = date?.toString();
+  if (!dateString) return '';
+  return isDateFormatted(dateString) ? slashedDate(dateString)
+    : compactDate(dateString);
 };
 
 // clean each date field
@@ -69,18 +67,17 @@ const cleanDate = (col, datas) => {
   headers.splice(idx, 1, col);
 
   const final = datas.map((row, i) => {
+    // First element is the header: skip it
     if (i === 0) {
       return row;
     }
 
-    const passAge = !row[idx] && col === 'BIRTHD' ? row[age] : null;
-
-    const date = (!row[idx] || row[idx]?.toString().length < 8) && col !== 'BIRTHD'
-      ? '1970-01-01' : col === 'BIRTHD'
-      && row[idx]?.toString().length < 8 ? emptyBirthDate(row[idx], passAge)
-        : formatDates(row[idx], passAge);
-
-    row.splice(idx, 1, date);
+    // Birth dates are formatted here
+    if (col === 'BIRTHD') {
+      row.splice(idx, 1, birthDates(row[idx], row[age]));
+    } else {
+      row.splice(idx, 1, formatDates(row[idx]));
+    }
 
     return row;
   });
